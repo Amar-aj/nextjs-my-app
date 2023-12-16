1c986af20cadafe59bd21614963f03b7f471f325
Amar-aj <amar.aj.github@gmail.com>
2023-12-16 21:30:11 +0530

    theme complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-
+import {LightDarkMode} from '../components/ui/theme'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -38,7 +38,7 @@ export default function Home() {
           priority
         />
       </div>
-
+        <LightDarkMode/>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
